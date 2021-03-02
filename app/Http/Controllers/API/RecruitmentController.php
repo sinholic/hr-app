18e0d9c7e57a389e07c15c300be8d7e64321a503
Mahar Prasetio <mp.prasetio@gmail.com>
2021-03-02 18:20:22 +0700

    fix api

--- app/Http/Controllers/API/RecruitmentController.php
+++ app/Http/Controllers/API/RecruitmentController.php
@@ -17,33 +17,48 @@ class RecruitmentController extends Controller
      */
     public function open()
     {
-        $recruitments = Recruitment::with(
-            'department',
-            'user_requested',
-            'user_change_status',
-            'user_processed',
-            'priority',
-            'request_status',
-            'process_status',
-            'candidates',
-            'candidates.candidate_status'
-        )
-        ->whereHas('process_status', function($query_process){
-            return $query_process->whereNotIn('name', [
-                'DONE',
-                'REJECTED'
-            ]);
-        })
-        ->whereHas('candidates.candidate_status', function($query_process){
-            return $query_process->whereNotIn('name', [
-                'NOT SUITABLE TO INTERVIEW',
-                'CV NOT SUITABLE',
-                'NOT SUITABLE FOR OL',
-                'NOT SUITABLE'
-            ]);
-        })
-        ->get();
+        $datas          =   \DB::select('SELECT 
+            DATE_FORMAT(rec.created_at, "%M %d, %Y") as created_at,
+            dept.`name` as department_name, 
+            job_position,
+            prt.`name` as priority,
+            reqs.`name` as request_status,
+            prcs.`name` as process_status,
+            COUNT(DISTINCT candpr.id) as number_of_proceed,
+            COUNT(DISTINCT candol.id) as number_of_ol_issued,
+            COUNT(DISTINCT candon.id) as number_of_on_board
+        FROM recruitments rec
+        JOIN `options` dept ON rec.department_id = dept.id
+        JOIN `options` prt ON rec.priority_id = prt.id
+        JOIN `options` reqs ON rec.request_status_id = reqs.id
+        JOIN `options` prcs ON rec.process_status_id = prcs.id
+        LEFT JOIN (
+            SELECT cand.id as id, cand.recruitment_id FROM candidates cand
+            JOIN `options` opt ON cand.candidate_status_id = opt.id 
+                        AND opt.`name` IN ("OFFERING LETTER SENT")
+        ) candol ON candol.recruitment_id = rec.id
+        LEFT JOIN(
+            SELECT cand.id as id, cand.recruitment_id FROM candidates cand
+            JOIN `options` opt ON cand.candidate_status_id = opt.id 
+                        AND opt.`name` IN ("ON BOARDING")
+        ) candon ON candon.recruitment_id = rec.id
+                LEFT JOIN (
+            SELECT cand.id as id, cand.recruitment_id FROM candidates cand
+            JOIN `options` opt ON cand.candidate_status_id = opt.id
+                        AND opt.`name` IN (
+                            "CV SUITABLE",
+                            "FORM SCREENING SENT",
+                            "FORM SCREENING RECEIVED",
+                            "SUITABLE TO INTERVIEW",
+                            "WAITING FOR INTERVIEW WITH USER",
+                            "WAITING FOR INTERVIEW WITH USER",
+                            "WAITING FOR USER\'S DECISION"
+                        )
+        ) candpr ON candpr.recruitment_id = rec.id
+                WHERE reqs.`name` NOT IN ("DONE", "REJECTED")
+        GROUP BY rec.id
+        ORDER BY FIELD(prt.`name`,"HIGH", "NORMAL", "LOW")');
 
-        return response()->json($recruitments, 200);
+        return response()->json($datas, 200);
     }
 }
